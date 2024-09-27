4d6b2b19aea17c3f1c351fbf5396a6bd70ff9683
Giacomo Rossettini <rossegiac@gmail.com>
2024-09-27 09:51:14 +0200

    start

---
+++ public/script.js
@@ -0,0 +1,8 @@
+let box = document.getElementById("ciaoste");
+let ciaoste = function () {
+  box.classList.toggle("translate-x-0");
+};
+let adios = document.getElementById("esc");
+let esc = function () {
+  box.classList.remove("translate-x-0");
+};
