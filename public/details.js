b6e737a4ca90818c36f56f433a039493a04bf938
Giacomo Rossettini <rossegiac@gmail.com>
2024-09-27 12:46:46 +0200

    card style

--- public/details.js
+++ public/details.js
@@ -12,7 +12,10 @@ const striveUrl = "https://striveschool-api.herokuapp.com/api/product/";
 const striveKey =
   "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmY2NmE3ZTc5YzQ1ZjAwMTU2OWI0YzYiLCJpYXQiOjE3Mjc0MjUxNTAsImV4cCI6MTcyODYzNDc1MH0.bPDY09eiQVeTddUNBt1560Knd0qh4be5T4i7uCqMzIU";
 
-fetch(striveUrl, {
+const address = new URLSearchParams(location.search);
+const productId = address.get("weaponId");
+
+fetch(`${striveUrl}/${productId}`, {
   headers: {
     Authorization: striveKey
   }
@@ -25,12 +28,17 @@ fetch(striveUrl, {
   })
   .then((data) => {
     console.log(data);
-    data.forEach((product) => {
-      let card = document.createElement("div");
-      card.innerHTML = `<img src=${product.imageUrl} alt=${product.name}>
-      <h2>${product.name}</h2>
-      <button onclick="window.location.href='./details.html?weaponId=${product._id}'">Vai ai dettagli</button>
-  `;
-      main.appendChild(card);
-    });
+    let card = document.createElement("div");
+    card.style.backgroundImage = `url(${data.imageUrl})`;
+    card.style.backgroundSize = "contain";
+    card.style.backgroundRepeat = "no-repeat";
+    card.style.backgroundPosition = "center center";
+    card.style.minHeight = "400px";
+    card.style.backgroundColor = "black";
+    let details = document.createElement("div");
+    details.style.alignItems = "start";
+    details.style.padding = "1rem";
+    details.innerHTML = `<h1 class="text-5xl font-black">${data.name}</h1>
+    <h2 class="text-sm">${data.description}</h2>`;
+    main.append(card, details);
   });
